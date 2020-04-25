271399bc56ad63e66fd7bd9603fbb923c032524b
SadikshaAryal1 <62279008+SadikshaAryal1@users.noreply.github.com>
2020-04-25 19:05:51 +0700

    index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ header('location:AddSA.php');
 <http>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Library System Example</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -108,7 +108,6 @@ table {
 
 <?php
 require('connect.php');
-$result = $conn->query("select * from loan");
 ?>
 
 <table border="2" align="center">
